0c614c663bbc88b1010fd3fb4e1d0af134618b51
Viktor Kostadinov <viktor.kostadinov@gmail.com>
2020-04-07 12:54:25 +0200

    Rework Instructions printout - languages implemented

--- src/Instruction.js
+++ src/Instruction.js
@@ -1,15 +1,17 @@
 import React, {useState, useEffect}  from 'react';
+import {useSelector} from "react-redux";
 
 export function Instruction(props)
 {
     const {data} = props;
+    const language = useSelector(state => state.language);
     return (
         <div>
             <div>
                 <span>Posted by: {data.createdBy} on {data.createdAt}</span>
                 { (data.createdAt !== data.updatedAt) ? <span>, updated at {data.updatedAt}</span> : "" }
             </div>
-            <div>{data.contents}</div>
+            <div>{data.contents[language]}</div>
             <hr/>
         </div>
     );
